5722feed12ad354e1d41023d8f5cc246fd138a83
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-13 18:37:35 -0400

    drawn simple black circle on canvas, had to comment out pipe, for some reason overwriting-?

--- site/js/main.js
+++ site/js/main.js
@@ -1,10 +1,10 @@
 var flappyBird = require('./flappy_bird');
-var pipe = require('./pipe');
+// var pipe = require('./pipe');
 
 document.addEventListener('DOMContentLoaded', function() {
   var app = new flappyBird.FlappyBird();
   app.run();
 
-  var pipes = new pipe.Pipe();
-  pipes.run();
+  // var pipes = new pipe.Pipe();
+  // pipes.run();
 });
