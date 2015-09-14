a93894d9e4fc2078da20492b0fec1b8e9a46406d
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-14 14:18:55 -0400

    tubes appear even if code is removed - ?

--- site/js/systems/pipes.js
+++ site/js/systems/pipes.js
@@ -6,38 +6,17 @@ var PipesSystem = function(entities) {
 };
 
 PipesSystem.prototype.run = function() {
-  // Run the update loop
   window.setInterval(this.tick.bind(this), 2000);
 };
 
 PipesSystem.prototype.tick = function() {
-  var right = 2 * this.canvas.width / this.canvas.height;
-  var gapPosition = 0.4 + Math.random() * 0.2;
 
-  var height = gapPosition - 0.15 / 2;
+  this.entities.push(new pipe.Pipe(position));
+
   var position = {
-      x: right + 0.15 / 2,
-      y: height / 2
   };
 
-  var size = {
-      x: 0.15,
-      y: height
-  };
-
-  this.entities.push(new pipe.Pipe(position, size));
-
-  var height = 1 - gapPosition - 0.15 / 2;
-  var position = {
-      x: right + 0.15 / 2,
-      y: 1 - height / 2
-  };
-
-  var size = {
-      x: 0.15,
-      y: height
-  };
-  this.entities.push(new pipe.Pipe(position, size));
+  this.entities.push(new pipe.Pipe(position));
 };
         
 exports.PipesSystem = PipesSystem;
