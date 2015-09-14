702126348c6d6b7b5707902fb2da86e63f17f266
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-13 20:37:18 -0400

    trying out things with canvas

--- site/js/components/graphics/bird.js
+++ site/js/components/graphics/bird.js
@@ -4,7 +4,15 @@ var BirdGraphicsComponent = function(entity) {
 
 BirdGraphicsComponent.prototype.draw = function(context) {
     context.beginPath();
+    context.strokeStyle = "blue"
+    context.arc(10, 10, 10, 0, 2 * Math.PI);
+    context.arc(30, 30, 10, 0, 2 * Math.PI);
     context.arc(50, 50, 10, 0, 2 * Math.PI);
+    context.arc(70, 70, 10, 0, 2 * Math.PI);
+    context.arc(90, 90, 10, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "red"
+    context.arc(110, 110, 10, 0, 2 * Math.PI);
     context.fill();
     console.log("Drawing a bird");
 };
